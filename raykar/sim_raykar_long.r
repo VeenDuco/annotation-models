18817968326bf63fcef9f328b65984d73a8acf78
Veen <d.veen@uu.nl>
2019-03-12 14:51:04 -0400

    Update sim_raykar_long.r

--- raykar/sim_raykar_long.r
+++ raykar/sim_raykar_long.r
@@ -1,16 +1,13 @@
+# settings
 library(rstan)
-options(mc.cores = parallel::detectCores())
+# options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 # model_predictors <- stan_model("raykar/raykar_marginal_long.stan")
 # saveRDS(model_predictors, "raykar/raykar_marginal_long.RDS")
 model_predictors <- readRDS("raykar/raykar_marginal_long.RDS")
+
+# define functions
 inv_logit <- function(u) 1 / (1 + exp(-u))
-
-# SSE_twostep <- NULL
-# SSE_direct <- NULL
-# nsim <- 10
-set.seed(13031990)
-
 data.simulation <- function(I, K, J, D, missing){
   succes <- FALSE
   while(!succes){
@@ -56,6 +53,44 @@ data.simulation <- function(I, K, J, D, missing){
   
 }
 
+# set n simulations
+nsim <- 2
+J = 10
+I = 500
+# create output for simulations
+predictors.overview <- list(
+  simulated.predictors = matrix(NA, nrow = 21, ncol = nsim),
+  direct_estimated = matrix(NA, nrow = 21, ncol = nsim),
+  two_step_estimated = matrix(NA, nrow = 21, ncol = nsim)
+)
+
+annotator.overview <- list(
+  true_anno_sens = matrix(NA, nrow = J, ncol = nsim),
+  direct_lb_sens = matrix(NA, nrow = J, ncol = nsim),
+  direct_mean_sens = matrix(NA, nrow = J, ncol = nsim),
+  direct_ub_sens = matrix(NA, nrow = J, ncol = nsim),
+  two_step_lb_sens = matrix(NA, nrow = J, ncol = nsim),
+  two_step_mean_sens = matrix(NA, nrow = J, ncol = nsim),
+  two_step_ub_sens = matrix(NA, nrow = J, ncol = nsim),
+  true_anno_spec = matrix(NA, nrow = J, ncol = nsim),
+  direct_lb_spec = matrix(NA, nrow = J, ncol = nsim),
+  direct_mean_spec = matrix(NA, nrow = J, ncol = nsim),
+  direct_ub_spec = matrix(NA, nrow = J, ncol = nsim),
+  two_step_lb_spec = matrix(NA, nrow = J, ncol = nsim),
+  two_step_mean_spec = matrix(NA, nrow = J, ncol = nsim),
+  two_step_ub_spec = matrix(NA, nrow = J, ncol = nsim)
+)
+
+z_overview <- list(
+  z_sim = matrix(NA, nrow = I, ncol = nsim),
+  direct_z_hat = matrix(NA, nrow = I, ncol = nsim),
+  two_step_z_hat = matrix(NA, nrow = I, ncol = nsim)
+)
+
+
+set.seed(13031990)
+for(sim in 1:nsim){
+  
 sim.data <- data.simulation(I = 500, K = 2, J = 10, D = 20, missing  = .5)
 
 w_init <- rnorm(20)
@@ -82,7 +117,6 @@ fit_DS_predictors <- sampling(object = model_predictors, data = list(J = sim.dat
 
 
 
-
 # same model blank predictors.
 fit_DS_blank_predictors <- sampling(object = model_predictors, data = list(J = sim.data$J,
                                                                            I = sim.data$I,
@@ -98,6 +132,42 @@ z_hat <- ifelse(summary(fit_DS_blank_predictors, pars = "E_z")$summary[, 1] < 0.
 
 fit.glm <- rstanarm::stan_glm(z_hat ~ 1 + sim.data$x, family = binomial())
 
+# predictor output
+predictors.overview$simulated.predictors[, sim] <- c(sim.data$w0, sim.data$w)
+predictors.overview$direct_estimated[, sim] <- summary(fit_DS_predictors, pars = c("w0", "w"))$summary[, 1]
+predictors.overview$two_step_estimated[, sim] <- summary(fit.glm)[1:21, 1]
+
+
+annotator.overview$true_anno_sens[, sim] <- sim.data$alpha
+annotator.overview$direct_lb_sens[, sim] <- summary(fit_DS_predictors, pars = c("alpha"))$summary[, 4]
+annotator.overview$direct_mean_sens[, sim] <- summary(fit_DS_predictors, pars = c("alpha"))$summary[, 1]
+annotator.overview$direct_ub_sens[, sim] <- summary(fit_DS_predictors, pars = c("alpha"))$summary[, 8]
+annotator.overview$two_step_lb_sens[, sim] <- summary(fit_DS_blank_predictors, pars = c("alpha"))$summary[, 4]
+annotator.overview$two_step_mean_sens[, sim] <- summary(fit_DS_blank_predictors, pars = c("alpha"))$summary[, 1]
+annotator.overview$two_step_ub_sens[, sim] <- summary(fit_DS_blank_predictors, pars = c("alpha"))$summary[, 8]
+annotator.overview$true_anno_spec[, sim] <- sim.data$beta
+annotator.overview$direct_lb_spec[, sim] <- summary(fit_DS_predictors, pars = c("beta"))$summary[, 4]
+annotator.overview$direct_mean_spec[, sim] <- summary(fit_DS_predictors, pars = c("beta"))$summary[, 1]
+annotator.overview$direct_ub_spec[, sim] <- summary(fit_DS_predictors, pars = c("beta"))$summary[, 8]
+annotator.overview$two_step_lb_spec[, sim] <- summary(fit_DS_blank_predictors, pars = c("beta"))$summary[, 4]
+annotator.overview$two_step_mean_spec[, sim] <- summary(fit_DS_blank_predictors, pars = c("beta"))$summary[, 1]
+annotator.overview$two_step_ub_spec[, sim] <- summary(fit_DS_blank_predictors, pars = c("beta"))$summary[, 8]
+                         
+z_overview$z_sim[, sim] <- sim.data$z
+z_overview$direct_z_hat[, sim] <- ifelse(summary(fit_DS_predictors, pars = "E_z")$summary[, 1] < 0.5, 0, 1)
+z_overview$two_step_z_hat[, sim] <- ifelse(summary(fit_DS_blank_predictors, pars = "E_z")$summary[, 1] < 0.5, 0, 1)
+
+}
+
+
+
+# bias_direct <- c(sim.data$w0, sim.data$w) - summary(fit_DS_predictors, pars = c("w0", "w"))$summary[, 1]
+# bias_two_step <- c(sim.data$w0, sim.data$w) - summary(fit.glm)[1:21, 1]
+# 
+# 
+# plot(bias_direct, bias_two_step, xlim = c(-2, 2), ylim = c(-2, 2))
+# abline(a = 0, b = 1)
+# abline(a = 0, b = -1)
 # SSE_twostep[sim] <- sum((summary(fit.glm)[1:21,c(1)] - c(w0, w))^2)
 # SSE_direct[sim] <- sum((summary(fit_DS_predictors, pars = c("w0", "w"))$summary[, c(1)] - c(w0, w))^2)
 
